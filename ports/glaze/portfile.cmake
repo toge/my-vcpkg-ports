821a5e118095a7867b4855d63976ed2411739efb
toge <toge.mail@gmail.com>
2026-03-17 11:02:31 +0900

    [glaze] update to 7.2.1

---
+++ ports/glaze/portfile.cmake
@@ -0,0 +1,31 @@
+if(VCPKG_TARGET_IS_LINUX)
+    message("Warning: `glaze` requires Clang15+ or GCC 12+ on Linux")
+endif()
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO stephenberry/glaze
+    REF "v${VERSION}"
+    SHA512 5783ee6d3abee59c89d0ee9488ff71acd95ff938335bff60dc858c282381608397491ec6bfe5a535349e54b2933dac68493b1adb32782a29fcd56ad97ae1b925
+    HEAD_REF main
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        ssl     glaze_ENABLE_SSL
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        ${FEATURE_OPTIONS}
+        -Dglaze_DEVELOPER_MODE=OFF
+        -Dglaze_BUILD_EXAMPLES=OFF
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")

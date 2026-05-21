85e8e7cc20bb4a42338b1809a2924d577fb9cc68
toge <toge.mail@gmail.com>
2026-05-22 00:30:11 +0900

    [frozenchars] update to 2026-05-22

--- ports/frozenchars/portfile.cmake
+++ ports/frozenchars/portfile.cmake
@@ -3,8 +3,8 @@ set(VCPKG_BUILD_TYPE release) # header only library
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO toge/frozenchars
-    REF 90e13654bfb6e627d94639be6c82aac032d67733
-    SHA512 852d99744c4248b57ae4b75b5a62ab8a2ffa5f8b3b583253a6f5abbb47a2471e505b8455c772d94d8e8c735eef9e489bdb66c7190f51d210e37c048755717c07
+    REF c49c74dbdf06de003f0f04c8dc8e8cffacaa1e3b
+    SHA512 d207a36a491088f4810bde6ef32bb8a6e693775a61258ee0c95f841e5c480dd29d88b3bbbdd6998b3b3bca49ffe069cb454aa97fd684e3797ff8b49c188a74aa
     HEAD_REF main
 )
 

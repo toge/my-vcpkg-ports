446301fc0202ae2427887980241b3b5b05929b9f
toge <toge.mail@gmail.com>
2026-06-06 03:11:46 +0900

    [lexborpp] update to 2026-06-06

--- ports/lexborpp/portfile.cmake
+++ ports/lexborpp/portfile.cmake
@@ -3,8 +3,8 @@ set(VCPKG_BUILD_TYPE release) # header only library
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO toge/lexborpp
-    REF 9aaca5c565c80c02cc4fd2008440bcfc17ea9cee
-    SHA512 f1d175220c090f52e2c6822afa6d8b97c30e1ba5351e563c9848dbd75cf93856c9fe96106279b8905bb16f216a0e56ec352a349fe3bea7fc648d9838b2bc2131
+    REF f698cac728cc01b0433f226ce8f24dd73b9e930d
+    SHA512 e8a7d19fc132d9cd37045f14d33e03cccce742d6defe0fe834a6d298af23be74559e48000cb062bfd174aed380a45fd6eaddf961a15d0eacbaeb33744b430e7f
     HEAD_REF main
 )
 

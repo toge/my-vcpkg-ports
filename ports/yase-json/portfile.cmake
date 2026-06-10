68b19585863fc341e16585e0ae08d7573e436776
toge <toge.mail@gmail.com>
2026-06-11 01:23:23 +0900

    [yase-json] update to 2026-06-10

--- ports/yase-json/portfile.cmake
+++ ports/yase-json/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO toge/yase-json
-    REF b19cb5ece7769326795c7abc42f8599bd11e25e0
-    SHA512 042f7a6da3373936d54a92db9bbfc72d950211f46438beced242ebfc508172428197a43b00fb05e88fcef90dc4138dacb60b54201183cb6298dc1fa914597e07
+    REF e57dae020a96ae685ae0cd129cf51691537431ce
+    SHA512 5398208fbbc52cfd537972dfa40fd0396f9adbddfa499c2018e4acb173129965e2f2c1b78672a70a50fd4285871f4a3dbf7a0e03cb115908b8a56ab95086127a
     HEAD_REF main
 )
 
@@ -10,6 +10,8 @@ set(VCPKG_BUILD_TYPE release) # header-only port
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DENABLE_TEST=OFF
 )
 
 vcpkg_cmake_install()

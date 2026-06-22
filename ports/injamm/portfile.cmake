2d61be2c5225f01823884e6da42102fa1dd05fb0
toge <toge.mail@gmail.com>
2026-06-22 17:17:38 +0900

    [injamm] update to 2026-06-22

--- ports/injamm/portfile.cmake
+++ ports/injamm/portfile.cmake
@@ -3,9 +3,14 @@ set(VCPKG_BUILD_TYPE release) # header only library
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO toge/injamm
-    REF 9db47554ec09eb99dcd5755e3280f9552ef199da
-    SHA512 b13475bee7199ea3fcce20f7cfd9f9e6b17e98dfc1fe5163251bacbc4447a648a4bee3035fe87a241cb5495adc96f86d2af45110e66b8d8ae495d4c6f797eef4
+    REF 79c6249aa6c9050b73ddfb8b8fcb4eab20456e8e
+    SHA512 48c7b55f96f67d7059e4b9c351f62c8d8a3d9fd76971a9158d9eca57902e18738ea758ddecd59acf758e5ed5522be987f4210bf8f60572be9fb03c421f4ccf0e
     HEAD_REF main
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        sqlite ENABLE_EXT_SQLITE3
 )
 
 vcpkg_cmake_configure(
@@ -13,11 +18,12 @@ vcpkg_cmake_configure(
     OPTIONS
         -DBUILD_TEST=OFF
         -DBUILD_EXAMPLE=OFF
+        ${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/injamm")
-
+vcpkg_cmake_config_fixup(PACKAGE_NAME injamm-sqlite3 CONFIG_PATH "lib/cmake/injamm-sqlite3")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")

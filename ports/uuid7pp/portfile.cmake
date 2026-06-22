28f39ca8083388f1d59645aa338e58d761f0e369
toge <toge.mail@gmail.com>
2026-06-23 02:53:51 +0900

    [uuid7pp] update to 2026-06-23

--- ports/uuid7pp/portfile.cmake
+++ ports/uuid7pp/portfile.cmake
@@ -3,15 +3,16 @@ set(VCPKG_BUILD_TYPE release) # header only library
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO toge/uuid7pp
-    REF 3fbc16da6fdd0df2fc76af9a34996ff731247709
-    SHA512 f194d92908f3974fc03c17451bef6015972bc16f2f26c025c27b2c5b0b6170e0028f8cc12b36870bec44525495142c15cca7cebb8295d67972141db20e97657f
+    REF 7e654cb66ccc03edbdad082c8ecb48b6821ce05e
+    SHA512 05fbcb976997050458a16cff49ded1e7a7d4cffc137058a9b0c56043fed335ce533263db3362ecdbe93aa52bfbcf702213859334325665a6f888b6410be32852
     HEAD_REF main
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DENABLE_TEST=OFF
+        -DBUILD_TEST=OFF
+        -DBUILD_SAMPLE=OFF
 )
 
 vcpkg_cmake_install()

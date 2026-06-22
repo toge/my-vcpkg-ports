f7576e31a46b909904956e0a5afaa1b6aab6d6aa
toge <toge.mail@gmail.com>
2026-06-23 02:01:28 +0900

    [xlsdraw] add new port

---
+++ ports/xlsdraw/portfile.cmake
@@ -0,0 +1,23 @@
+set(VCPKG_BUILD_TYPE release) # header only library
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO toge/xlsdraw
+    REF 1f8d7651946d2624a183d33112e06afd1bf2722e
+    SHA512 189876bdfdec0294498deaaf65155e24e788dba90f2a8ecefc8fb047e88b12b32d18cb1fe03e6fae4e67a82474eccf1830ad8e94d76f7858b9deb785306c9395
+    HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_TEST=OFF
+        -DBUILD_EXAMPLE=OFF
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/xlsdraw")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")

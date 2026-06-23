2e68f096e11456a46582dd765e0ceb64e19d0b82
toge <toge.mail@gmail.com>
2026-06-23 10:15:40 +0900

    [zxcpp] update to 2026-06-23

--- ports/zxcpp/portfile.cmake
+++ ports/zxcpp/portfile.cmake
@@ -3,14 +3,16 @@ set(VCPKG_BUILD_TYPE release) # header only library
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO toge/zxcpp
-    REF bcc7b7001b274a8981c4322a2048b4cde6877962
-    SHA512 eee6264ef70e21c185469d161b5f83dc7aa238cd0f9237c023dcfb51ff8364c5f8ec44d214022cac0fedefcf3a9ea1889977370f090876c89bb9571f1b3c3544
+    REF 6f24582ad930536759d8af81c9739fb9e749ed6f
+    SHA512 6437eb7dd37c3ae3ec33e1f8b27fde6c42195d3c547591ad79a49c7301b3708f03425e7b5c892b98ca2a2e58de23175f2d3fb154afd9373d23ded74e34e6a92c
     HEAD_REF main
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-)
+   OPTIONS
+        -DBUILD_TEST=OFF
+ )
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/zxcpp")

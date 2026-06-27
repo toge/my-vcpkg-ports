3115f33501dbcdab3aa944f4dfdada9c8f919945
toge <toge.mail@gmail.com>
2026-06-27 18:41:45 +0900

    [ladybugdb-ladybug] add -Wl,-z,noseparate-code for shared lib ELF layout

--- ports/ladybugdb-ladybug/portfile.cmake
+++ ports/ladybugdb-ladybug/portfile.cmake
@@ -16,12 +16,18 @@ vcpkg_check_features(
         shell BUILD_SHELL
 )
 
+set(EXTRA_OPTIONS "")
+if(VCPKG_TARGET_IS_LINUX AND CMAKE_HOST_SYSTEM_PROCESSOR MATCHES "x86_64|amd64|AMD64")
+    list(APPEND EXTRA_OPTIONS "-DCMAKE_SHARED_LINKER_FLAGS=-Wl,-z,noseparate-code")
+endif()
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         ${FEATURE_OPTIONS}
         -DBUILD_SINGLE_FILE_HEADER=OFF
         -DBUILD_TESTS=FALSE
+        ${EXTRA_OPTIONS}
 )
 
 vcpkg_cmake_install()

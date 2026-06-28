c82484082c050e0c1b0eb5aa275728f841fde9e7
toge <toge.mail@gmail.com>
2026-06-28 10:06:41 +0900

    [ladybugdb-ladybug] fix _init segfault via LINK_FLAGS -Wl,-z,noseparate-code
    binutils 2.46 ld.bfd が -z separate-code を既定で使用する際、
    liblbug.so の .init セクションを非実行 RW セグメントに配置しつつ
    _init シンボル値を古い仮想アドレスで残す不整合が発生し、ローダが
    DT_INIT を呼んだ時点で SIGSEGV していた。
    
    vcpkg toolchain が CMAKE_SHARED_LINKER_FLAGS を上書きするため、
    ターゲットプロパティ LINK_FLAGS 経由で -Wl,-z,noseparate-code を
    渡して .init を実行可能セグメントに配置する。

--- ports/ladybugdb-ladybug/portfile.cmake
+++ ports/ladybugdb-ladybug/portfile.cmake
@@ -8,6 +8,7 @@ vcpkg_from_github(
         unofficial-cmake-config.patch
         include-cstdint.patch
         fix-alp-encode-avx512.patch
+        fix-init-segment.patch
 )
 
 vcpkg_check_features(
@@ -16,22 +17,12 @@ vcpkg_check_features(
         shell BUILD_SHELL
 )
 
-if(VCPKG_TARGET_IS_LINUX AND VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    # binutils 2.46 ld.bfd の -z separate-code 既定で .init セクションが
-    # 非実行セグメントに配置され _init シンボルと不整合となり、ローダが
-    # DT_INIT を呼んだ時点で SIGSEGV する問題を回避する。
-    set(EXTRA_LINKER_FLAGS "-DCMAKE_SHARED_LINKER_FLAGS=-Wl,-z,noseparate-code")
-else()
-    set(EXTRA_LINKER_FLAGS "")
-endif()
-
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         ${FEATURE_OPTIONS}
         -DBUILD_SINGLE_FILE_HEADER=OFF
         -DBUILD_TESTS=FALSE
-        ${EXTRA_LINKER_FLAGS}
 )
 
 vcpkg_cmake_install()

c4ff41986d825da8a21234d015ba273ab62bacb5
toge <toge.mail@gmail.com>
2026-07-02 19:24:48 +0900

    [frozenchars] add minify_cypher operator

--- ports/frozenchars/portfile.cmake
+++ ports/frozenchars/portfile.cmake
@@ -3,8 +3,8 @@ set(VCPKG_BUILD_TYPE release) # header only library
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO toge/frozenchars
-    REF d394ec5217517eb0f48492b44e1d66188ee52780
-    SHA512 9ebc7ec5eb56db28af1c278d1ce3aed7a8ed510892e4dc10eea3beff5c75faf8546b2682c74d16c613b25feda73c40941e4c3da109311efc23e3551dd70865ed
+    REF 72fa9954fbd05a121f441572478861064573f062
+    SHA512 40b17f041f34d0fff325ed33bdc4c3a9fa47b029f438e966b1ede125cd229f6180685e5dcfaf2f5500e44c1a3a537f28dd1a5e32c8df0f9625b2a9d1809fccf7
     HEAD_REF main
 )
 

61453e798cf97038708676f60285f30acdfb0661
toge <toge.mail@gmail.com>
2026-07-02 15:37:54 +0900

    [glz-sql] update to 2026-07-01

--- ports/glz-sql/portfile.cmake
+++ ports/glz-sql/portfile.cmake
@@ -3,19 +3,19 @@ set(VCPKG_BUILD_TYPE release) # header only library
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO toge/glz-sql
-    REF bb7d403b2162cbb8c454e6be157b61211f4a563e
-    SHA512 c1625f1ba9f05b503560db803e1e4b73556330ac595dd0be0477223a008b84bebc2d15267660cd921c61f0878234da75bb3b6ce542c6635a3c0ca640e9e793b4
+    REF 8a0073bb78b9c628c4bc801ce5f1ad19f5879341
+    SHA512 6f7a615887f12214d929fdb76fee965f5d3506d7f3df55b92b400aac2121d6bccb3cb5690f7bfc5f727fcecb03e1127c562cefea17554434acceb482b68c4da1
     HEAD_REF main
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DENABLE_TESTS=OFF
+        -DBUILD_TEST=OFF
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME glaze-sql CONFIG_PATH "lib/cmake/glaze-sql")
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/glz-sql")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
 

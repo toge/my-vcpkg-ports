5831e9a1fd1e2d7801aa78e6fd998dfe20514310
toge <toge.mail@gmail.com>
2026-07-05 23:17:26 +0900

    [pcrepp] update to 2026-07-05

--- ports/pcrepp/portfile.cmake
+++ ports/pcrepp/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO toge/pcrepp
-    REF ec5201a1386e2fbb83a26f22ec1b4a7ef61860e9
-    SHA512 f61a93f488242f6e83c37b9fdc1a670e4f9e9d778f52bfc4a5a49243aa631de5261528ead4ff40ecfd28caa6800a61968e6d0da88c3fae00dc98dac1950e8b05
+    REF a70454ba53417b6da513a4cb7f84f74f35547bc5
+    SHA512 81856500d06d96c35a59bc040b42456017fdd0f90eb74ed88b806cf9dcd84b12e9884b04986d952e78a4e35cc5cdfeaf40d59fe244d4fc9cd809bbf38b717fa5
     HEAD_REF main
 )
 
@@ -11,10 +11,18 @@ vcpkg_replace_string("${SOURCE_PATH}/CMakeLists.txt"
     "write_basic_package_version_file(\"\${CMAKE_CURRENT_BINARY_DIR}/pcreppConfigVersion.cmake\" VERSION 0.1.0 COMPATIBILITY AnyNewerVersion)\nconfigure_package_config_file("
 )
 
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        ctre    WITH_CTRE
+        glaze   WITH_GLAZE
+)
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DENABLE_TESTS=OFF
+        -DBUILD_TEST=OFF
+        -DBUILD_BENCH=OFF
+        ${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()

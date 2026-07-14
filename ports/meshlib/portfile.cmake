71278078729631953adc378160581f5f63b4f19b
toge <toge.mail@gmail.com>
2026-07-14 13:50:02 +0900

    [meshlib] make smaller ports

--- ports/meshlib/portfile.cmake
+++ ports/meshlib/portfile.cmake
@@ -19,6 +19,12 @@ set(ENV{PKG_CONFIG} "${PKGCONFIG}")
 # meshlib supports only dynamic linking
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 
+if("symbolmesh" IN_LIST FEATURES)
+  set(SYMBOLMESH_FLAG ON)
+else()
+  set(SYMBOLMESH_FLAG OFF)
+endif()
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     DISABLE_PARALLEL_CONFIGURE
@@ -27,24 +33,27 @@ vcpkg_cmake_configure(
         -DBUILD_TESTING=OFF
         -DMR_CXX_STANDARD=20
         -DMR_PCH=OFF
+        -DMRMESH_NO_GTEST=ON
+        -DMRMESH_NO_TIFF=ON
+        -DCMAKE_INSTALL_INCLUDEDIR:STRING=include
+        -DCMAKE_INSTALL_LIBDIR:STRING=lib
+        # Build only the MRMesh core (geometry + basic I/O); drop heavy features.
         -DMESHLIB_PYTHON_SUPPORT=OFF
-        -DMESHLIB_BUILD_MRCUDA=OFF
+        -DMESHLIB_BUILD_PYTHON_MODULES=OFF
         -DMESHLIB_BUILD_MESHVIEWER=OFF
         -DMESHLIB_BUILD_MRVIEWER=OFF
-        -DMESHLIB_BUILD_PYTHON_MODULES=OFF
-        -DMESHLIB_USE_VCPKG=ON
-        -DMRMESH_NO_GTEST=ON
+        -DMESHLIB_BUILD_MESHCONV=OFF
+        -DMESHLIB_BUILD_VOXELS=OFF
+        -DMESHLIB_BUILD_EXTRA_IO_FORMATS=OFF
+        -DMESHLIB_BUILD_MCP=OFF
+        -DMESHLIB_BUILD_MRCUDA=OFF
+        -DMESHLIB_BUILD_SYMBOLMESH=${SYMBOLMESH_FLAG}
 )
 
 vcpkg_cmake_install()
 
 vcpkg_fixup_pkgconfig()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/MeshLib)
-
-vcpkg_copy_tools(
-    TOOL_NAMES meshconv
-    AUTO_CLEAN
-)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share"
                     "${CURRENT_PACKAGES_DIR}/debug/include"

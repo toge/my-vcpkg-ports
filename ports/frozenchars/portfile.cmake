a64db34942cd55a333c7b6644b2b26409eb6bd77
toge <toge.mail@gmail.com>
2026-07-26 22:57:39 +0900

    [frozenchars] update to 0.3.0

--- ports/frozenchars/portfile.cmake
+++ ports/frozenchars/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO toge/frozenchars
     REF v${VERSION}
-    SHA512 f6bc0e0f416495e3e8c1893ab2feed62c08ded6f0aeb5f3ae12a5be7ce9c7cd77cb2549a0875ad5f93e33bb5836b98513aac84bb49431d56279befc680803b66
+    SHA512 473f909ef556d84e4292bebbcec58dd2aba2e32c2376e6f050036aa5db1460d6eceb64ca7e5c866ba9ded08ea023286228ac81e65290549fd4d966cab318b6b7
     HEAD_REF main
 )
 

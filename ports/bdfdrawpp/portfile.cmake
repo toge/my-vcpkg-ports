1c8a15350c6ad67effc46f53e8b904986714b0f8
toge <toge.mail@gmail.com>
2026-07-30 18:06:48 +0900

    [bdfdrawpp] fix cmake target

--- ports/bdfdrawpp/portfile.cmake
+++ ports/bdfdrawpp/portfile.cmake
@@ -21,6 +21,6 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/bdfpp")
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/bdfdrawpp")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")

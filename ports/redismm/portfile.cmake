22ef761663524b4a9827bcbd1abe2ee7ec90d9c4
toge <toge.mail@gmail.com>
2026-08-02 23:53:17 +0900

    [redismm] add new port

---
+++ ports/redismm/portfile.cmake
@@ -0,0 +1,21 @@
+set(VCPKG_BUILD_TYPE release) # header only library
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO toge/redismm
+    REF 388f4535afce753c00e109909be138c90b285acc
+    SHA512 79e3e6e18c1cfaf2b2433352ab3f39465ad2dedcaab115ce20a16314e14d4c79bad42e217e23a631fe60a67339e9be26797b3f2b8d3c0c99a63a2fff56fbcded
+    HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_TEST=OFF
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/redismm")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")

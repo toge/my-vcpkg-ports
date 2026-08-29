862b1df059dda21ddcdd60090671125d8b415387
toge <toge@example.com>
2026-08-30 01:29:14 +0900

    fix(laz-perf): copyright インストールと debug クリーンアップ追加
    上流に LICENSE あり。debug/include と debug/share も削除。PACKAGE_NAME LAZPERF は上流のケースに準拠。

--- ports/laz-perf/portfile.cmake
+++ ports/laz-perf/portfile.cmake
@@ -16,3 +16,7 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(PACKAGE_NAME LAZPERF CONFIG_PATH lib/cmake/LAZPERF)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")

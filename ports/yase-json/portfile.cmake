a51886c41251dcbe52ba6c4046a87009fcd7c573
toge <toge@example.com>
2026-08-30 01:31:43 +0900

    fix(yase-json): debug/include と debug/share のクリーンアップを追加
    Header-only port was only removing lib.

--- ports/yase-json/portfile.cmake
+++ ports/yase-json/portfile.cmake
@@ -17,6 +17,6 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/yase-json)
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share" "${CURRENT_PACKAGES_DIR}/lib")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")

e5f1f43750473e8651fbbb47421afeba5113c689
toge <toge@example.com>
2026-08-30 01:31:08 +0900

    fix(redismm): debug/share クリーンアップを追加
    Header-only port was only removing debug/include.

--- ports/redismm/portfile.cmake
+++ ports/redismm/portfile.cmake
@@ -16,6 +16,6 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/redismm")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")

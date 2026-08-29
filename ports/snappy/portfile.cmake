32e411cc7caa8dfa6eee86cf3d57e0dccf58c709
toge <toge@example.com>
2026-08-30 01:33:29 +0900

    chore(snappy): usage ファイルを file(INSTALL) に変更

--- ports/snappy/portfile.cmake
+++ ports/snappy/portfile.cmake
@@ -39,6 +39,6 @@ vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")

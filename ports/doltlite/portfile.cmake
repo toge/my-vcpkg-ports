936c78686825f02223ca07fbb31653fbe496f101
toge <toge.mail@gmail.com>
2026-08-30 23:17:25 +0900

    [doltlite] update to 0.11.55

--- ports/doltlite/portfile.cmake
+++ ports/doltlite/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_download_distfile(ARCHIVE
     URLS "https://github.com/dolthub/doltlite/releases/download/v${VERSION}/doltlite-amalgamation-${VERSION}.zip"
     FILENAME "doltlite-amalgamation-${VERSION}.zip"
-    SHA512 7784ade039b5961442537dfccbb623ad332cce87868a42517d15eef86c6d9de015266a6e63766ef7da547a6c67edfef0e3214f382c4bbade4ea1d4670b03cee1
+    SHA512 d8648b47527336dd10f66036d11c4bdc5b670ca919602bc3bab23e78a0de43aae24f629f2a2c0322285deeb73be270b74aeeb77ec9da1bd5304e339b739763a8
 )
 
 vcpkg_extract_source_archive(
@@ -24,7 +24,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-doltlite CONFIG_PATH lib/cmake/unofficial-doltlite)
+vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-doltlite)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
@@ -33,7 +33,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH LICENSE_PATH
     REPO dolthub/doltlite
     REF "v${VERSION}"
-    SHA512 f8f5bb04c78f84e5aca5ed452448ccf218d06df0b1b174949b3992c41a6808a60210bf692244ce1571174f1343daf6f235f324ab641d37f885b9f556495b084f
+    SHA512 d89c751dd2e13b8cd07bd3dd7a94ba7cb47bf043dd0f87761b051c813aa247e8d5dedf1f11a0f3600ac0a947a1ee679adc7558b7ffe1be3353cebe4e4bb9181d
     HEAD_REF master
 )
 

928aa644d73674f79e43c7755d0458198ef03aa5
toge <toge.mail@gmail.com>
2026-08-30 23:04:35 +0900

    [budouxpp] update to 2026-06-23

--- ports/budouxpp/portfile.cmake
+++ ports/budouxpp/portfile.cmake
@@ -3,15 +3,16 @@ set(VCPKG_BUILD_TYPE release) # header only library
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO toge/budouxpp
-    REF 667b4a0ff8c5fdf2ee87e528d592c567e4a51699
-    SHA512 d064e3456ceea6783cc08ad3d653f7f186248a99c7fdfdbdb465a9526ea6738ceb972e576e4ba0c040cd5ff1c3208ed902180bfed42531c1bcb551cf3ea0107a
+    REF b7e5ee88fe9d89282e2e2aee1d7ef5356f57b7d3
+    SHA512 86a4efbaf6cdbbc976756723dbb273ad2b4d3a0782f45ac952b86d17cdbbba23cd97639ce660176c5979f43e784ebcb915cff228a121e73101c0b214391d27ef
     HEAD_REF main
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DENABLE_TESTS=OFF
+        -DBUILD_TEST=OFF
+        -DBUILD_EXAMPLE=OFF
 )
 
 vcpkg_cmake_install()
@@ -20,5 +21,3 @@ vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/budouxpp")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
-
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")

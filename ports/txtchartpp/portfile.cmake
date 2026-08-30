f0b17ad59eb88359269c785b3c4b76f2fe25e23d
toge <toge.mail@gmail.com>
2026-08-31 01:22:15 +0900

    [txtchartpp] fix port-version position

--- ports/txtchartpp/portfile.cmake
+++ ports/txtchartpp/portfile.cmake
@@ -18,5 +18,3 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/txtchartpp")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
-
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")

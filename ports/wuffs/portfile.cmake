ab14dfa87ee166b71fe8ae8e780da59f4b7f98ab
toge <toge.mail@gmail.com>
2026-08-31 11:28:01 +0900

    [wuffs] update to 0.3.5

--- ports/wuffs/portfile.cmake
+++ ports/wuffs/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO google/wuffs
     REF v${VERSION}
-    SHA512 a9313ecea49c2d7e003daac685ca9bdd114bf7e70ea14ec0f2d6049c3fae9c0b1861e37fce4b014ca738822558ab346a95d66f2cde6b8db45cdd6d1c46be4856
+    SHA512 413d86137b30bcb4a73c25e3e68beccb7a46d735da58a6027ac64e42da04e552cb49e3d923e657ccbf9dba0e6253eedde913b2a7a46d183e631ec573b6258f7f
     HEAD_REF main
 )
 
